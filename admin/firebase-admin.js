42c4c46cab45281e42c980177294ffbe7a7ef3f8
JiriPallas <57175963+JiriPallas@users.noreply.github.com>
2025-05-03 15:48:34 +0200

    next try

--- admin/firebase-admin.js
+++ admin/firebase-admin.js
@@ -1,4 +1,15 @@
-// firebase-admin.js – används av admin/index.html
-import { db, pushData, isValidItem } from './modules/firebase-init.js';
+// firebase-admin.js – används i admin/index.html
+import { db, pushData } from './modules/firebase-init.js';
+
+function isValidItem(item) {
+  return (
+    item.imageUrl &&
+    item.category &&
+    item.type &&
+    item.description &&
+    item.fromDate &&
+    item.toDate
+  );
+}
 
 export { db, pushData, isValidItem };
